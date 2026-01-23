0090c478cda526a3a527ab5f1e594d5b93998fb0
Brandon Belz <brandonbelz@icloud.com>
2026-01-23 15:59:50 -0500

    Fleshed out physics API

--- physics.hpp
+++ physics.hpp
@@ -1,6 +1,7 @@
 #ifndef PHYSICS_H
 #define PHYSICS_H
 
+#include <utility>
 #include <vector>
 
 class DVector2 {
@@ -9,34 +10,41 @@ class DVector2 {
 
 public:
   DVector2(double x, double y);
-  ~DVector2();
-};
-
-class SimObject {
-  double mass;
-  float radius;
-  DVector2 position;
-  DVector2 velocity;
-
-public:
-  SimObject();
-  ~SimObject();
+  double get_x() const;
+  double get_y() const;
 };
 
 class Simulation {
+  class SimObject {
+    double mass;
+    float radius;
+    DVector2 position;
+    DVector2 velocity;
+
+  public:
+    SimObject(double mass, float radius, DVector2 position, DVector2 velocity);
+    SimObject(double mass, float radius, DVector2 position);
+
+    double get_mass() const;
+    float get_radius() const;
+    DVector2 get_position() const;
+    DVector2 get_velocity() const;
+
+    void set_position(DVector2 position);
+    void set_velocity(DVector2 velocity);
+  };
   std::vector<SimObject> objects;
 
 public:
   Simulation();
-  ~Simulation();
-  void add(double mass, float radius, DVector2 position);
-  int remove(int object_id);
+  int add(double mass, float radius, DVector2 position);
+  std::pair<int, int> remove(int object_id);
 
-  DVector2 get_position(int object_id);
-  double get_mass(int object_id);
-  DVector2 get_velocity(int object_id);
-  float get_radius(int object_id);
-  std::vector<DVector2> get_positions();
+  DVector2 get_position(int object_id) const;
+  double get_mass(int object_id) const;
+  DVector2 get_velocity(int object_id) const;
+  float get_radius(int object_id) const;
+  const std::vector<DVector2> &get_positions() const;
 
   void set_position(int object_id, DVector2 position);
   void set_mass(int object_id, double mass);
