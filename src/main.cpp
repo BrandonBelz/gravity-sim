d1d0b3f89e91cbe7cefd958edb65d6e690131f01
Brandon Belz <belzbrandonm@gmail.com>
2026-03-16 10:48:14 -0500

    Switch to mouse wheel camera zooming

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,18 @@
 #include <vector>
 
 int main(void) {
-    std::vector<PhysicalObject> objs;
-    objs.push_back((PhysicalObject){5.972E24, 6371000.0, DVector2(0.0, 0.0),
-                                    DVector2(0.0, -0.0125), RGB(0, 0, 255)});
-    objs.push_back((PhysicalObject){7.348E22, 1737000.0, DVector2(384400000.0, 0.0),
-                                    DVector2(0.0, 1018.0), RGB(130, 130, 130)});
+  std::vector<PhysicalObject> objs;
+  objs.push_back((PhysicalObject){5.972E24, 6.371E6, DVector2(0.0, 0.0),
+                                  DVector2(0.0, -0.0125), RGB(0, 0, 255)});
+  objs.push_back((PhysicalObject){7.348E22, 1.737E6, DVector2(3.844E8, 0.0),
+                                  DVector2(0.0, 1018.0), RGB(130, 130, 130)});
 
-    auto physicsHandler = std::make_unique<PhysicsHandler>(1000.0, objs);
-    physicsHandler->setGravityStrat(std::make_unique<RealGravity>());
-    GraphicsHandler *window = new RaylibHandler(std::move(physicsHandler));
-    window->setZoom(0.000002);
-    window->setSpeed(80000.0);
-    window->run();
-    delete window;
-    return 0;
+  auto physicsHandler = std::make_unique<PhysicsHandler>(1000.0, objs);
+  physicsHandler->setGravityStrat(std::make_unique<RealGravity>());
+  GraphicsHandler *window = new RaylibHandler(std::move(physicsHandler));
+  window->setZoom(2E-6);
+  window->setSpeed(8E4);
+  window->run();
+  delete window;
+  return 0;
 }
--- src/raylib_wrap.cpp
+++ src/raylib_wrap.cpp
@@ -4,110 +4,112 @@
 #include <vector>
 
 typedef struct {
-    double speed;
-    bool paused;
+  double speed;
+  bool paused;
 } SpeedState;
 
 typedef struct {
-    SpeedState speedState;
-    Camera2D cam;
-    float frameTime;
-    int screenWidth;
-    int screenHeight;
-    const std::vector<PhysicalObject> &objects;
+  SpeedState speedState;
+  Camera2D cam;
+  float frameTime;
+  int screenWidth;
+  int screenHeight;
+  const std::vector<PhysicalObject> &objects;
 } SimState;
 
 void applyCamMove(Camera2D &cam) {
-    Vector2 target_change = {0.0f, 0.0f};
-    if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
-        target_change.y -= 10.0f;
-    }
-    if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {
-        target_change.y += 10.0f;
-    }
-    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
-        target_change.x += 10.0f;
-    }
-    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
-        target_change.x -= 10.0f;
-    }
-    cam.target.x += target_change.x / cam.zoom;
-    cam.target.y += target_change.y / cam.zoom;
+  Vector2 target_change = {0.0f, 0.0f};
+  if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
+    target_change.y -= 10.0f;
+  }
+  if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {
+    target_change.y += 10.0f;
+  }
+  if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
+    target_change.x += 10.0f;
+  }
+  if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
+    target_change.x -= 10.0f;
+  }
+  cam.target.x += target_change.x / cam.zoom;
+  cam.target.y += target_change.y / cam.zoom;
 }
 
 void applyCamZoom(Camera2D &cam) {
-    if (IsKeyDown(KEY_EQUAL)) {
-        cam.zoom *= 1.1f;
+  int mouseMove = GetMouseWheelMove();
+  if (mouseMove > 0) {
+    for (int i = 0; i < mouseMove; i++) {
+      cam.zoom *= 1.3;
     }
-    if (IsKeyDown(KEY_MINUS)) {
-        cam.zoom /= 1.1f;
+  } else if (mouseMove < 0) {
+    for (int i = 0; i > mouseMove; i--) {
+      cam.zoom /= 1.3;
     }
+  }
 }
 
 void applySpeedChanges(SpeedState &speedState) {
-    if (IsKeyPressed(KEY_SPACE)) {
-        speedState.paused = !speedState.paused;
-    }
+  if (IsKeyPressed(KEY_SPACE)) {
+    speedState.paused = !speedState.paused;
+  }
 }
 
 void handleInput(SimState &state) {
-    applyCamMove(state.cam);
-    applyCamZoom(state.cam);
-    applySpeedChanges(state.speedState);
+  applyCamMove(state.cam);
+  applyCamZoom(state.cam);
+  applySpeedChanges(state.speedState);
 }
 
 void RaylibHandler::run() {
-    SimState state = {
-        {simSpeed, false}, (Camera2D){0},     0.0f,
-        GetScreenWidth(),  GetScreenHeight(), physicsHandler->get_objects()};
+  SimState state = {
+      {simSpeed, false}, (Camera2D){0},     0.0f,
+      GetScreenWidth(),  GetScreenHeight(), physicsHandler->get_objects()};
 
-    InitWindow(1920, 1080, "Gravity!");
-    SetWindowState(FLAG_WINDOW_RESIZABLE);
-    SetTargetFPS(60);
+  InitWindow(1920, 1080, "Gravity!");
+  SetWindowState(FLAG_WINDOW_RESIZABLE);
+  SetTargetFPS(60);
 
+  state.cam.offset = {state.screenWidth / 2.0f, state.screenHeight / 2.0f};
+  state.cam.zoom = zoom;
+
+  while (!WindowShouldClose()) {
+    state.frameTime = GetFrameTime();
+    state.screenWidth = GetScreenWidth();
+    state.screenHeight = GetScreenHeight();
     state.cam.offset = {state.screenWidth / 2.0f, state.screenHeight / 2.0f};
-    state.cam.zoom = zoom;
 
-    while (!WindowShouldClose()) {
-        state.frameTime = GetFrameTime();
-        state.screenWidth = GetScreenWidth();
-        state.screenHeight = GetScreenHeight();
-        state.cam.offset = {state.screenWidth / 2.0f,
-                            state.screenHeight / 2.0f};
+    BeginDrawing();
+    handleInput(state);
 
-        BeginDrawing();
-        handleInput(state);
+    if (!state.speedState.paused)
+      physicsHandler->step(state.frameTime * state.speedState.speed);
+    ClearBackground(BLACK);
+    BeginMode2D(state.cam);
 
-        if (!state.speedState.paused)
-            physicsHandler->step(state.frameTime * state.speedState.speed);
-        ClearBackground(BLACK);
-        BeginMode2D(state.cam);
+    Vector2 topLeft = GetScreenToWorld2D({0, 0}, state.cam);
+    Vector2 botRight =
+        GetScreenToWorld2D({static_cast<float>(state.screenWidth),
+                            static_cast<float>(state.screenHeight)},
+                           state.cam);
 
-        Vector2 topLeft = GetScreenToWorld2D({0, 0}, state.cam);
-        Vector2 botRight =
-            GetScreenToWorld2D({static_cast<float>(state.screenWidth),
-                                static_cast<float>(state.screenHeight)},
-                               state.cam);
+    Rectangle inView = {topLeft.x, topLeft.y, botRight.x - topLeft.x,
+                        botRight.y - topLeft.y};
 
-        Rectangle inView = {topLeft.x, topLeft.y, botRight.x - topLeft.x,
-                            botRight.y - topLeft.y};
-
-        for (PhysicalObject obj : state.objects) {
-            Rectangle objBox = {
-                static_cast<float>(obj.pos.get_x() - obj.radius),
-                static_cast<float>(obj.pos.get_y() - obj.radius),
-                static_cast<float>(obj.radius * 2),
-                static_cast<float>(obj.radius * 2)};
-            if (CheckCollisionRecs(inView, objBox)) {
-                Color color = {(u_char)obj.color.get_red(),
-                               (u_char)obj.color.get_green(),
-                               (u_char)obj.color.get_blue(), 255};
-                DrawCircle(obj.pos.get_x(), obj.pos.get_y(), obj.radius, color);
-            }
-        }
-
-        EndMode2D();
-        EndDrawing();
+    for (PhysicalObject obj : state.objects) {
+      Rectangle objBox = {static_cast<float>(obj.pos.get_x() - obj.radius),
+                          static_cast<float>(obj.pos.get_y() - obj.radius),
+                          static_cast<float>(obj.radius * 2),
+                          static_cast<float>(obj.radius * 2)};
+      if (CheckCollisionRecs(inView, objBox)) {
+        Color color = {(u_char)obj.color.get_red(),
+                       (u_char)obj.color.get_green(),
+                       (u_char)obj.color.get_blue(), 255};
+        DrawCircle(obj.pos.get_x(), obj.pos.get_y(), obj.radius, color);
+      }
     }
-    CloseWindow();
+
+    EndMode2D();
+    EndDrawing();
+  }
+  CloseWindow();
 }
